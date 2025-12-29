cad3abde30451a056433a09608920559e6f8fbad
Ayush Sahu <115640976+ayushS22@users.noreply.github.com>
2025-12-29 18:52:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function nextPalindrome(num) {
-  //your JS code here.
+  // convert input to number (prompt gives string)
+  num = Number(num);
+
+  // keep checking next numbers
+  while (true) {
+    num++; // move to next number
+
+    // convert number to string
+    let str = num.toString();
+
+    // reverse the string
+    let reversed = str.split('').reverse().join('');
+
+    // check palindrome
+    if (str === reversed) {
+      return num;
+    }
+  }
 }
 
+// Do not change below code
 const input = prompt("Enter a palindrome number");
 alert(nextPalindrome(input));
